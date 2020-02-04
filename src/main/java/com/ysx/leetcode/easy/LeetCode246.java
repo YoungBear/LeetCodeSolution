63c38c6b2775dd8220d40a572eed9d74ed3b7ce8
youngbear <youngbear@aliyun.com>
2020-02-04 14:33:00 +0800

    LeetCode 246. 中心对称数
    Signed-off-by: youngbear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode246.java
@@ -0,0 +1,43 @@
+package com.ysx.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/2 13:43
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 246. 中心对称数
+ * https://leetcode-cn.com/problems/strobogrammatic-number/
+ * 6 -> 9
+ * 9 -> 6
+ * 1 -> 1
+ * 8 -> 8
+ * 0 -> 0
+ */
+public class LeetCode246 {
+
+    private static final Map<Character, Character> MAP = new HashMap<>();
+
+    static {
+        MAP.put('0', '0');
+        MAP.put('1', '1');
+        MAP.put('6', '9');
+        MAP.put('8', '8');
+        MAP.put('9', '6');
+    }
+
+    public boolean isStrobogrammatic(String num) {
+        for (int i = 0; i < num.length() / 2 + 1; i++) {
+            if (!MAP.containsKey(num.charAt(i))) {
+                return false;
+            }
+            if (MAP.get(num.charAt(i)) != num.charAt(num.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode246Test.java
@@ -0,0 +1,34 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/2 13:44
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode246Test {
+
+    private LeetCode246 leetCode246;
+
+    @Before
+    public void init() {
+        leetCode246 = new LeetCode246();
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertTrue(leetCode246.isStrobogrammatic("69"));
+        Assert.assertTrue(leetCode246.isStrobogrammatic("88"));
+        Assert.assertFalse(leetCode246.isStrobogrammatic("962"));
+        Assert.assertTrue(leetCode246.isStrobogrammatic("0"));
+        Assert.assertTrue(leetCode246.isStrobogrammatic("1"));
+        Assert.assertFalse(leetCode246.isStrobogrammatic("2"));
+    }
+
+}
