5e9a6f017fa67efe9e3306abb7085ba992a9ea63
youngbear <youngbear@aliyun.com>
2020-02-12 09:32:59 +0800

    LeetCode 443. 压缩字符串
    Signed-off-by: youngbear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode443.java
@@ -0,0 +1,45 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/12 09:27
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 443. 压缩字符串
+ * https://leetcode-cn.com/problems/string-compression/
+ */
+public class LeetCode443 {
+    public int compress(char[] chars) {
+        int length = chars.length;
+        int cur = 0;
+        for (int i = 0; i < length; i++) {
+            char ch = chars[i];
+            chars[cur++] = ch;
+            for (int j = i + 1; j < length; j++) {
+                if (ch != chars[j]) {
+                    if (j == i + 1) {
+                        break;
+                    }
+                    char[] tempChars = String.valueOf(j - i).toCharArray();
+                    for (char tempChar : tempChars) {
+                        chars[cur++] = tempChar;
+                    }
+                    i = j - 1;
+                    break;
+                } else {
+                    // 最后一个字符
+                    if (j == length - 1) {
+                        char[] tempChars = String.valueOf(j - i + 1).toCharArray();
+                        for (char tempChar : tempChars) {
+                            chars[cur++] = tempChar;
+                        }
+                        i = j;
+                        break;
+                    }
+                }
+            }
+        }
+        return cur;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode443Test.java
@@ -0,0 +1,30 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/12 09:28
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode443Test {
+    @Test
+    public void test() {
+        LeetCode443 leetCode443 = new LeetCode443();
+        char[] chars1 = {'a','a','b','b','c','c','c'};
+        int num1 = 6;
+        Assert.assertEquals(num1, leetCode443.compress(chars1));
+
+        char[] chars2 = {'a'};
+        int num2 = 1;
+        Assert.assertEquals(num2, leetCode443.compress(chars2));
+
+        char[] chars3 = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+        int num3 = 4;
+        Assert.assertEquals(num3, leetCode443.compress(chars3));
+    }
+}
