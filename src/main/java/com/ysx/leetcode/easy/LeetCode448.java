1af9891fcfc537bfc9e4375b7bd214c7f07b2f1a
YoungBear <youngbear@aliyun.com>
2020-02-24 23:03:05 +0800

    LeetCode 448. 找到所有数组中消失的数字
    将数组中的每个值所对应的下标的值，修改为对应的负值，完成之后，再找到值为正数的下标即可
    
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode448.java
@@ -0,0 +1,36 @@
+package com.ysx.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.abs;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/24 22:50
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 448. 找到所有数组中消失的数字
+ * https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
+ */
+public class LeetCode448 {
+    /**
+     * 将数组中的每个值所对应的下标的值，修改为对应的负值，完成之后，再找到值为正数的下标即可
+     *
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int n : nums) {
+            nums[abs(n) - 1] = -abs(nums[abs(n) - 1]);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode448Test.java
@@ -0,0 +1,32 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/24 22:51
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode448Test {
+    private LeetCode448 leetCode448;
+
+    @Before
+    public void init() {
+        leetCode448 = new LeetCode448();
+    }
+
+    @Test
+    public void test1() {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> result = Arrays.asList(5, 6);
+        Assert.assertEquals(result, leetCode448.findDisappearedNumbers(nums));
+    }
+}
