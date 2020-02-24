eb4006b2cea4289001f99a4a9b1fdd25f6a9230d
YoungBear <youngbear@aliyun.com>
2020-02-24 22:40:01 +0800

    LeetCode 1133. 最大唯一数
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1133.java
@@ -0,0 +1,36 @@
+package com.ysx.leetcode.easy;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/24 22:27
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1133. 最大唯一数
+ * https://leetcode-cn.com/problems/largest-unique-number/
+ */
+public class LeetCode1133 {
+    public int largestUniqueNumber(int[] A) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int a : A) {
+            if (map.containsKey(a)) {
+                map.put(a, map.get(a) + 1);
+            } else {
+                map.put(a, 1);
+            }
+        }
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        int result = -1;
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            if (entry.getValue() == 1) {
+                result = entry.getKey();
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1133Test.java
@@ -0,0 +1,36 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/24 22:35
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1133Test {
+    private LeetCode1133 leetCode1133;
+
+    @Before
+    public void init() {
+        leetCode1133 = new LeetCode1133();
+    }
+
+    @Test
+    public void test1() {
+        int[] A = {5, 7, 3, 9, 4, 9, 8, 3, 1};
+        int largest = leetCode1133.largestUniqueNumber(A);
+        Assert.assertEquals(8, largest);
+    }
+
+    @Test
+    public void test2() {
+        int[] A = {9, 9, 8, 8};
+        int largest = leetCode1133.largestUniqueNumber(A);
+        Assert.assertEquals(-1, largest);
+    }
+}
