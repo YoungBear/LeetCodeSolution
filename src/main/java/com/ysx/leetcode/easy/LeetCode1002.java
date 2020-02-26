5a4d1c88fa48d3821cba403594871d8c9b321ea0
YoungBear <youngbear@aliyun.com>
2020-02-26 21:43:40 +0800

    LeetCode 1002. 查找常用字符
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1002.java
@@ -0,0 +1,44 @@
+package com.ysx.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/26 21:23
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1002. 查找常用字符
+ * https://leetcode-cn.com/problems/find-common-characters/
+ */
+public class LeetCode1002 {
+    public List<String> commonChars(String[] A) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : A[0].toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 1; i < A.length; i++) {
+            Map<Character, Integer> tempMap = new HashMap<>();
+            for (char ch : A[i].toCharArray()) {
+                tempMap.put(ch, tempMap.getOrDefault(ch, 0) + 1);
+            }
+            for (char ch : map.keySet()) {
+                if (tempMap.containsKey(ch)) {
+                    map.put(ch, Math.min(map.get(ch), tempMap.get(ch)));
+                } else {
+                    map.put(ch, 0);
+                }
+            }
+        }
+        List<String> result = new ArrayList<>();
+        for (char ch : map.keySet()) {
+            for (int i = 0; i < map.get(ch); i++) {
+                result.add(String.valueOf(ch));
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1002Test.java
@@ -0,0 +1,35 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/2/26 21:23
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1002Test {
+    @Test
+    public void test1() {
+        LeetCode1002 leetCode1002 = new LeetCode1002();
+        String[] A = {"bella","label","roller"};
+        List<String> stringList = leetCode1002.commonChars(A);
+        List<String> exceptedList = Arrays.asList("e","l","l");
+        Assert.assertEquals(exceptedList, stringList);
+    }
+
+    @Test
+    public void test2() {
+        LeetCode1002 leetCode1002 = new LeetCode1002();
+        String[] A = {"cool","lock","cook"};
+        List<String> stringList = leetCode1002.commonChars(A);
+        List<String> exceptedList = Arrays.asList("c","o");
+        Assert.assertEquals(exceptedList, stringList);
+    }
+}
