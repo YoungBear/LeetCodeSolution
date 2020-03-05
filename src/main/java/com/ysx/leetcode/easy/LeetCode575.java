1bbc51f14f1582347698bfe4f364f790fbd5607b
YoungBear <youngbear@aliyun.com>
2020-03-05 22:10:03 +0800

    LeetCode 575. 分糖果
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode575.java
@@ -0,0 +1,26 @@
+package com.ysx.leetcode.easy;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/5 22:06
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 575. 分糖果
+ * https://leetcode-cn.com/problems/distribute-candies/
+ */
+public class LeetCode575 {
+    /**
+     * 找到数组中唯一元素的长度x，和数组总长度的一半 y，取Math.min(x,y)即可
+     * 如果唯一元素大于一半(x>y)，则可从x中取全部元素的一半分给妹妹，返回为y
+     * 如果唯一元素小于或者等于一半(x<=y)，则把全部唯一的元素分给妹妹，再加上其余的，总体妹妹手里的糖果的种类是x
+     *
+     * @param candies
+     * @return
+     */
+    public int distributeCandies(int[] candies) {
+        return (int) Math.min(IntStream.of(candies).distinct().count(), candies.length / 2);
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode575Test.java
@@ -0,0 +1,30 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/5 22:07
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode575Test {
+    @Test
+    public void distributeCandiesTest1() {
+        LeetCode575 leetCode575 = new LeetCode575();
+        int[] candies = {1, 1, 2, 2, 3, 3};
+        int excepted = 3;
+        Assert.assertEquals(excepted, leetCode575.distributeCandies(candies));
+    }
+
+    @Test
+    public void distributeCandiesTest2() {
+        LeetCode575 leetCode575 = new LeetCode575();
+        int[] candies = {1, 1, 2, 3};
+        int excepted = 2;
+        Assert.assertEquals(excepted, leetCode575.distributeCandies(candies));
+    }
+}
