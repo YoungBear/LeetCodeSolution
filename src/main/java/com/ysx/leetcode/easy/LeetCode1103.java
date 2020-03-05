ee9378df842652d467de0f33418efdc027a44b45
YoungBear <youngbear@aliyun.com>
2020-03-05 22:17:55 +0800

    LeetCode 1103. 分糖果 II
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1103.java
@@ -0,0 +1,42 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/5 22:11
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1103. 分糖果 II
+ * https://leetcode-cn.com/problems/distribute-candies-to-people/
+ */
+public class LeetCode1103 {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] array = new int[num_people];
+        // 共有m个，即1,2,3,...,m
+        int m = (int) Math.sqrt(2 * candies);
+        for (int i = 0; i < 10; i++) {
+            if (m * (m + 1) / 2 <= candies) {
+                m++;
+            } else {
+                break;
+            }
+        }
+        int last = candies - m * (m - 1) / 2;
+        int lastLineNumber = m % num_people;
+        int height = m / num_people;
+        if (height * num_people == m) {
+            height--;
+            lastLineNumber = num_people;
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = height * (i + 1) + height * (height - 1) * num_people / 2;
+        }
+        // 处理最后一行
+        for (int i = 0; i < lastLineNumber - 1; i++) {
+            array[i] += height * num_people + (i + 1);
+        }
+        // 处理最后一个元素
+        array[lastLineNumber - 1] += last;
+        return array;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1103Test.java
@@ -0,0 +1,32 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/5 22:13
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1103Test {
+    @Test
+    public void distributeCandiesTest1() {
+        LeetCode1103 leetCode1103 = new LeetCode1103();
+        int candies = 7;
+        int num_people = 4;
+        int[] excepted = {1, 2, 3, 1};
+        Assert.assertArrayEquals(excepted, leetCode1103.distributeCandies(candies, num_people));
+    }
+
+    @Test
+    public void distributeCandiesTest2() {
+        LeetCode1103 leetCode1103 = new LeetCode1103();
+        int candies = 10;
+        int num_people = 3;
+        int[] excepted = {5, 2, 3};
+        Assert.assertArrayEquals(excepted, leetCode1103.distributeCandies(candies, num_people));
+    }
+}
