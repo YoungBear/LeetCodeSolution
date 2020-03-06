0dfffcb48fec6a80d618d01f07bd256d75c46e54
YoungBear <youngbear@aliyun.com>
2020-03-06 22:37:40 +0800

    LeetCode 829. 连续整数求和``
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/advanced/LeetCode829.java
@@ -0,0 +1,27 @@
+package com.ysx.leetcode.advanced;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/6 22:34
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 829. 连续整数求和
+ * https://leetcode-cn.com/problems/consecutive-numbers-sum/
+ */
+public class LeetCode829 {
+    public int consecutiveNumbersSum(int N) {
+        int num = 0;
+        for (int k = (int) Math.sqrt(2 * N); k >= 1; k--) {
+            int a0 = (2 * N / k - k + 1) / 2;
+            if (a0 <= 0) {
+                // 不考虑0或者负数
+                continue;
+            }
+            if (N == ((k * k + 2 * a0 * k - k) / 2)) {
+                num++;
+            }
+        }
+        return num;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/advanced/LeetCode829Test.java
@@ -0,0 +1,30 @@
+package com.ysx.leetcode.advanced;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/6 22:35
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode829Test {
+    @Test
+    public void test1() {
+        LeetCode829 leetCode829 = new LeetCode829();
+        int N = 5;
+        int excepted = 2;
+        Assert.assertEquals(excepted, leetCode829.consecutiveNumbersSum(N));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode829 leetCode829 = new LeetCode829();
+        int N = 9;
+        int excepted = 3;
+        Assert.assertEquals(excepted, leetCode829.consecutiveNumbersSum(N));
+    }
+}
