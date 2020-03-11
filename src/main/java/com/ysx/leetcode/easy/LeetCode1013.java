4ca1f4b1ce5a55e805c534a3690cf445c152dd42
YoungBear <youngbear@aliyun.com>
2020-03-11 22:23:15 +0800

    LeetCode1013. 将数组分成和相等的三个部分
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1013.java
@@ -0,0 +1,50 @@
+package com.ysx.leetcode.easy;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/11 22:19
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1013. 将数组分成和相等的三个部分
+ * https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum/
+ */
+public class LeetCode1013 {
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum = IntStream.of(A).sum();
+        int a = sum / 3;
+        if (a * 3 != sum) {
+            return false;
+        }
+        int sum1 = 0;
+        int sum2 = 0;
+        boolean flag1 = true;
+        boolean flag2 = true;
+        for (int value : A) {
+            if (flag1) {
+                flag1 = false;
+                // 保证分组不为空，必须加添加一个元素
+                sum1 += value;
+                continue;
+            }
+            if (sum1 != a) {
+                sum1 += value;
+            } else {
+                if (flag2) {
+                    flag2 = false;
+                    // 保证分组不为空，必须加添加一个元素
+                    sum2 += value;
+                    continue;
+                }
+                if (sum2 != a) {
+                    sum2 += value;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1013Test.java
@@ -0,0 +1,36 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/11 22:21
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1013Test {
+    @Test
+    public void test1() {
+        LeetCode1013 leetCode1013 = new LeetCode1013();
+        int[] A = {0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1};
+        Assert.assertTrue(leetCode1013.canThreePartsEqualSum(A));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode1013 leetCode1013 = new LeetCode1013();
+        int[] A = {0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1};
+        Assert.assertFalse(leetCode1013.canThreePartsEqualSum(A));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode1013 leetCode1013 = new LeetCode1013();
+        int[] A = {3, 3, 6, 5, -2, 2, 5, 1, -9, 4};
+        Assert.assertTrue(leetCode1013.canThreePartsEqualSum(A));
+    }
+
+}
