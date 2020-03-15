841d719199811705f9240f009fea33bfe6718105
YoungBear <youngbear@aliyun.com>
2020-03-15 20:51:20 +0800

    LeetCode 1071. 字符串的最大公因子
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1071.java
@@ -0,0 +1,48 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/15 20:48
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1071. 字符串的最大公因子
+ * https://leetcode-cn.com/problems/greatest-common-divisor-of-strings/
+ */
+public class LeetCode1071 {
+    public String gcdOfStrings(String str1, String str2) {
+        String result = "";
+        for (int i = 0; i < str1.length() && i < str2.length(); i++) {
+            String factor = str1.substring(0, i + 1);
+            if (isFactor(str1, factor) && isFactor(str2, factor)) {
+                result = factor;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 判断factor是否为str的因子
+     * factor和str长度都大于0
+     *
+     * @param str
+     * @param factor
+     * @return
+     */
+    public boolean isFactor(String str, String factor) {
+        int strLength = str.length();
+        int factorLength = factor.length();
+        int n = strLength / factorLength;
+        if (n * factorLength != strLength) {
+            return false;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < factorLength; j++) {
+                if (factor.charAt(j) != str.charAt(i * factorLength + j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1071Test.java
@@ -0,0 +1,41 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/15 20:48
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1071Test {
+    @Test
+    public void test1() {
+        LeetCode1071 leetCode1071 = new LeetCode1071();
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        String excepted = "ABC";
+        Assert.assertEquals(excepted, leetCode1071.gcdOfStrings(str1, str2));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode1071 leetCode1071 = new LeetCode1071();
+        String str1 = "ABABAB";
+        String str2 = "ABAB";
+        String excepted = "AB";
+        Assert.assertEquals(excepted, leetCode1071.gcdOfStrings(str1, str2));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode1071 leetCode1071 = new LeetCode1071();
+        String str1 = "LEET";
+        String str2 = "CODE";
+        String excepted = "";
+        Assert.assertEquals(excepted, leetCode1071.gcdOfStrings(str1, str2));
+    }
+}
