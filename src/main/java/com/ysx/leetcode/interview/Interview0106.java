4d9fc2769272c9bd44ca315b3d109cc7ed007ca8
YoungBear <youngbear@aliyun.com>
2020-03-16 22:36:39 +0800

    Interview0106. 字符串压缩
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/interview/Interview0106.java
@@ -0,0 +1,37 @@
+package com.ysx.leetcode.interview;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/16 22:18
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 面试题 01.06. 字符串压缩
+ * https://leetcode-cn.com/problems/compress-string-lcci/
+ */
+public class Interview0106 {
+    public String compressString(String S) {
+        if (S == null || S.length() <= 1) {
+            return S;
+        }
+        char[] charArray = S.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < charArray.length; ) {
+            int num = 1;
+            int j;
+            for (j = i + 1; j < charArray.length; j++) {
+                if (charArray[i] != charArray[j]) {
+                    break;
+                }
+                num++;
+            }
+            sb.append(charArray[i]).append(num);
+            i = j;
+        }
+        if (S.length() <= sb.length()) {
+            return S;
+        } else {
+            return sb.toString();
+        }
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/interview/Interview0106Test.java
@@ -0,0 +1,30 @@
+package com.ysx.leetcode.interview;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/16 22:19
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class Interview0106Test {
+    @Test
+    public void test1() {
+        String S = "aabcccccaaa";
+        String excepted = "a2b1c5a3";
+        Interview0106 interview0106 = new Interview0106();
+        Assert.assertEquals(excepted, interview0106.compressString(S));
+    }
+
+    @Test
+    public void test2() {
+        String S = "abbccd";
+        String excepted = "abbccd";
+        Interview0106 interview0106 = new Interview0106();
+        Assert.assertEquals(excepted, interview0106.compressString(S));
+    }
+}
