29ff8954e640c47bb8885efc4da7090d33788bdf
YoungBear <youngbear@aliyun.com>
2020-03-18 23:06:31 +0800

    LeetCode 836. 矩形重叠
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode836.java
@@ -0,0 +1,21 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/18 22:32
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 836. 矩形重叠
+ * https://leetcode-cn.com/problems/rectangle-overlap/
+ */
+public class LeetCode836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // 逆向思维
+        // 两个矩形不相交，则 r2 可以在 r1 的上下左右任意一个位置
+        return !(rec2[1] >= rec1[3] || // 上
+                rec2[3] <= rec1[1] || // 下
+                rec2[2] <= rec1[0] || // 左
+                rec2[0] >= rec1[2]); // 右
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode836Test.java
@@ -0,0 +1,38 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/18 22:32
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode836Test {
+    @Test
+    public void test1() {
+        LeetCode836 leetCode836 = new LeetCode836();
+        int[] rec1 = {0, 0, 2, 2};
+        int[] rec2 = {1, 1, 3, 3};
+        Assert.assertTrue(leetCode836.isRectangleOverlap(rec1, rec2));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode836 leetCode836 = new LeetCode836();
+        int[] rec1 = {0, 0, 1, 1};
+        int[] rec2 = {1, 0, 2, 1};
+        Assert.assertFalse(leetCode836.isRectangleOverlap(rec1, rec2));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode836 leetCode836 = new LeetCode836();
+        int[] rec1 = {7, 8, 13, 15};
+        int[] rec2 = {10, 8, 12, 20};
+        Assert.assertTrue(leetCode836.isRectangleOverlap(rec1, rec2));
+    }
+}
