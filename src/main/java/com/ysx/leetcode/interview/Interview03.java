b16c6cd8dd1eb8d4f73c04ca32355716738ebd32
YoungBear <youngbear@aliyun.com>
2020-03-21 23:49:33 +0800

    Interview03. 数组中重复的数字
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/interview/Interview03.java
@@ -0,0 +1,28 @@
+package com.ysx.leetcode.interview;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/21 23:31
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 面试题03. 数组中重复的数字
+ * https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+ */
+public class Interview03 {
+    public int findRepeatNumber(int[] nums) {
+        // 把元素大小改为 1-n
+        // 参考 LeetCode442 https://leetcode-cn.com/problems/find-all-duplicates-in-an-array/
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]++;
+        }
+        for (int num : nums) {
+            if (nums[Math.abs(num)-1] < 0) {
+                return Math.abs(num) - 1;
+            } else {
+                nums[Math.abs(num) - 1] *= -1;
+            }
+        }
+        throw new RuntimeException("has no answer.");
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/interview/Interview03Test.java
@@ -0,0 +1,30 @@
+package com.ysx.leetcode.interview;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/21 23:35
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class Interview03Test {
+    @Test
+    public void test1() {
+        Interview03 interview03 = new Interview03();
+        int[] nums = {0, 1, 0};
+        int excepted = 0;
+        Assert.assertEquals(excepted, interview03.findRepeatNumber(nums));
+    }
+
+    @Test
+    public void test2() {
+        Interview03 interview03 = new Interview03();
+        int[] nums = {2, 3, 1, 0, 2, 5, 3};
+        int excepted = 2;
+        Assert.assertEquals(excepted, interview03.findRepeatNumber(nums));
+    }
+}
