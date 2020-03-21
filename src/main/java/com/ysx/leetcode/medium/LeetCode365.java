d8c042ecec7a223c4e5d9bfb4cc2befe3120765b
YoungBear <youngbear@aliyun.com>
2020-03-21 20:54:05 +0800

    LeetCode 365. 水壶问题
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode365.java
@@ -0,0 +1,41 @@
+package com.ysx.leetcode.medium;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/21 20:36
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 365. 水壶问题
+ * https://leetcode-cn.com/problems/water-and-jug-problem/
+ */
+public class LeetCode365 {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if (z > x + y) {
+            return false;
+        }
+        if (x == 0 || y == 0) {
+            return z == 0 || z == x + y;
+        }
+        return z % gcd(x, y) == 0;
+    }
+
+    /**
+     * 辗转相除法求 x 和 y 的最大公约数
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public int gcd(int x, int y) {
+        if (x < y) {
+            return gcd(y, x);
+        }
+        while (y != 0) {
+            int temp = x % y;
+            x = y;
+            y = temp;
+        }
+        return x;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/medium/LeetCode365Test.java
@@ -0,0 +1,39 @@
+package com.ysx.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/21 20:37
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode365Test {
+    @Test
+    public void test1() {
+        LeetCode365 leetCode365 = new LeetCode365();
+        int x = 3;
+        int y = 5;
+        int z = 4;
+        Assert.assertTrue(leetCode365.canMeasureWater(x, y, z));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode365 leetCode365 = new LeetCode365();
+        int x = 2;
+        int y = 6;
+        int z = 5;
+        Assert.assertFalse(leetCode365.canMeasureWater(x, y, z));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode365 leetCode365 = new LeetCode365();
+        Assert.assertEquals(2, leetCode365.gcd(2, 4));
+        Assert.assertEquals(1, leetCode365.gcd(3, 5));
+    }
+}
