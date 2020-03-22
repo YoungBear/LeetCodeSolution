2d6e639c0e883d3bf0f8244f643073ddfd069f93
YoungBear <youngbear@aliyun.com>
2020-03-22 14:02:29 +0800

    LeetCode 153. 寻找旋转排序数组中的最小值
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode153.java
@@ -0,0 +1,23 @@
+package com.ysx.leetcode.medium;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/22 13:46
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 153. 寻找旋转排序数组中的最小值
+ * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+public class LeetCode153 {
+    public int findMin(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                result = nums[i];
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/medium/LeetCode153Test.java
@@ -0,0 +1,46 @@
+package com.ysx.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/22 13:47
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode153Test {
+    @Test
+    public void test1() {
+        LeetCode153 leetCode153 = new LeetCode153();
+        int[] nums = {3, 4, 5, 1, 2};
+        int excepted = 1;
+        Assert.assertEquals(excepted, leetCode153.findMin(nums));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode153 leetCode153 = new LeetCode153();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int excepted = 0;
+        Assert.assertEquals(excepted, leetCode153.findMin(nums));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode153 leetCode153 = new LeetCode153();
+        int[] nums = {0, 1, 2};
+        int excepted = 0;
+        Assert.assertEquals(excepted, leetCode153.findMin(nums));
+    }
+
+    @Test
+    public void test4() {
+        LeetCode153 leetCode153 = new LeetCode153();
+        int[] nums = {4, 5, 6, 7, 0};
+        int excepted = 0;
+        Assert.assertEquals(excepted, leetCode153.findMin(nums));
+    }
+}
