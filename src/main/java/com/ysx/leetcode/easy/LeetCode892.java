cbe77091fc6f0dc02f5881c4454f1a2612501e24
YoungBear <youngbear@aliyun.com>
2020-03-25 22:22:59 +0800

    LeetCode 892. 三维形体的表面积
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode892.java
@@ -0,0 +1,41 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/25 22:04
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 892. 三维形体的表面积
+ * https://leetcode-cn.com/problems/surface-area-of-3d-shapes/
+ */
+public class LeetCode892 {
+    /**
+     * 先计算当前元素贡献的表面积，然后再减去相邻的两个元素重合的部分
+     * 时间复杂度为 O(N*N)
+     * 空间复杂度为O(1)
+     *
+     * @param grid
+     * @return
+     */
+    public int surfaceArea(int[][] grid) {
+        int area = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] > 0) {
+                    // 获取当前这个元素贡献的表面积，4 个侧面积和 2 个顶面和地面
+                    area += grid[i][j] * 4 + 2;
+                    // 减去和 grid[i-1][j] 重合的面积
+                    if (i > 0) {
+                        area -= Math.min(grid[i - 1][j], grid[i][j]) * 2;
+                    }
+                    // 减去和 grid[i][j-1] 重合的面积
+                    if (j > 0) {
+                        area -= Math.min(grid[i][j - 1], grid[i][j]) * 2;
+                    }
+                }
+            }
+        }
+        return area;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode892Test.java
@@ -0,0 +1,55 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/25 22:06
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode892Test {
+    @Test
+    public void test1() {
+        LeetCode892 leetCode892 = new LeetCode892();
+        int[][] grid = {{2}};
+        int excepted = 10;
+        Assert.assertEquals(excepted, leetCode892.surfaceArea(grid));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode892 leetCode892 = new LeetCode892();
+        int[][] grid = {{1, 2}, {3, 4}};
+        int excepted = 34;
+        Assert.assertEquals(excepted, leetCode892.surfaceArea(grid));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode892 leetCode892 = new LeetCode892();
+        int[][] grid = {{1, 0}, {0, 2}};
+        int excepted = 16;
+        Assert.assertEquals(excepted, leetCode892.surfaceArea(grid));
+    }
+
+    @Test
+    public void test4() {
+        LeetCode892 leetCode892 = new LeetCode892();
+        int[][] grid = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        int excepted = 32;
+        Assert.assertEquals(excepted, leetCode892.surfaceArea(grid));
+    }
+
+    @Test
+    public void test5() {
+        LeetCode892 leetCode892 = new LeetCode892();
+        int[][] grid = {{2, 2, 2}, {2, 1, 2}, {2, 2, 2}};
+        int excepted = 46;
+        Assert.assertEquals(excepted, leetCode892.surfaceArea(grid));
+    }
+
+}
