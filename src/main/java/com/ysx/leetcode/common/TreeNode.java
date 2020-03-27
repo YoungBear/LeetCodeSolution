32786a9e993a0f115ffab80798b15e6034332161
YoungBear <youngbear@aliyun.com>
2020-03-27 22:58:14 +0800

    LeetCode 102. 二叉树的层次遍历
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/common/TreeNode.java
@@ -0,0 +1,19 @@
+package com.ysx.leetcode.common;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/27 22:50
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode102.java
@@ -0,0 +1,50 @@
+package com.ysx.leetcode.medium;
+
+import com.ysx.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/3/27 22:44
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 102. 二叉树的层次遍历
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ */
+public class LeetCode102 {
+    /**
+     * 队列实现二叉树的层次遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        List<TreeNode> nodeList = new LinkedList<>();
+        nodeList.add(root);
+        while (nodeList.size() > 0) {
+            List<Integer> list = new ArrayList<>();
+            List<TreeNode> temp = new LinkedList<>();
+            for (TreeNode node : nodeList) {
+                list.add(node.val);
+                if (node.left != null) {
+                    temp.add(node.left);
+                }
+                if (node.right != null) {
+                    temp.add(node.right);
+                }
+            }
+            result.add(list);
+            nodeList.clear();
+            nodeList.addAll(temp);
+        }
+        return result;
+    }
+}
