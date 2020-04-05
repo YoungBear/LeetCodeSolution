6c2144a08333f6cced83c40f637f04d7a37239ad
youngbear <youngbear@aliyun.com>
2020-04-05 11:54:03 +0800

    LeetCode 617. 合并二叉树
    Signed-off-by: youngbear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode617.java
@@ -0,0 +1,27 @@
+package com.ysx.leetcode.easy;
+
+import com.ysx.leetcode.common.TreeNode;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/5 11:40
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 617. 合并二叉树
+ * https://leetcode-cn.com/problems/merge-two-binary-trees/
+ */
+public class LeetCode617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode treeNode = new TreeNode(t1.val + t2.val);
+        treeNode.left = mergeTrees(t1.left, t2.left);
+        treeNode.right = mergeTrees(t1.right, t2.right);
+        return treeNode;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode617Test.java
@@ -0,0 +1,47 @@
+package com.ysx.leetcode.easy;
+
+import com.ysx.leetcode.common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/5 11:44
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode617Test {
+    @Test
+    public void test1() {
+        LeetCode617 leetCode617 = new LeetCode617();
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t11 = new TreeNode(3);
+        TreeNode t12 = new TreeNode(2);
+        TreeNode t13 = new TreeNode(5);
+        t11.left = t13;
+        t1.left = t11;
+        t1.right = t12;
+
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t21 = new TreeNode(1);
+        TreeNode t22 = new TreeNode(3);
+        TreeNode t23 = new TreeNode(4);
+        TreeNode t24 = new TreeNode(7);
+        t21.right = t23;
+        t22.right = t24;
+        t2.left = t21;
+        t2.right = t22;
+
+        TreeNode t3 = leetCode617.mergeTrees(t1, t2);
+        Assert.assertEquals(3, t3.val);
+        Assert.assertEquals(4, t3.left.val);
+        Assert.assertEquals(5, t3.right.val);
+        Assert.assertEquals(5, t3.left.left.val);
+        Assert.assertEquals(4, t3.left.right.val);
+        Assert.assertEquals(7, t3.right.right.val);
+
+
+    }
+}
