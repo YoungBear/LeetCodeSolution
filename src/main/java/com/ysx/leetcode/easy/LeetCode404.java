81a0d6b1f2b75fd48540deaca3c7058a98825a0f
youngbear <youngbear@aliyun.com>
2020-04-07 22:45:46 +0800

    LeetCode 404. 左叶子之和
    Signed-off-by: youngbear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode404.java
@@ -0,0 +1,24 @@
+package com.ysx.leetcode.easy;
+
+import com.ysx.leetcode.common.TreeNode;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/7 22:44
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 404. 左叶子之和
+ * https://leetcode-cn.com/problems/sum-of-left-leaves/
+ */
+public class LeetCode404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
