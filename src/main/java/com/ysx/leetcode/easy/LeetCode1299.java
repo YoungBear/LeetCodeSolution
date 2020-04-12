593b8ad75d700113479edd98b0f051ba69e81048
YoungBear <youngbear@aliyun.com>
2020-04-12 19:40:09 +0800

    LeetCode 1299. 将每个元素替换为右侧最大元素
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1299.java
@@ -0,0 +1,25 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/12 19:12
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1299. 将每个元素替换为右侧最大元素
+ * https://leetcode-cn.com/problems/replace-elements-with-greatest-element-on-right-side/
+ */
+public class LeetCode1299 {
+    public int[] replaceElements(int[] arr) {
+        int[] result = new int[arr.length];
+        int max = arr[arr.length - 1];
+        result[arr.length - 1] = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i + 1] > max) {
+                max = arr[i + 1];
+            }
+            result[i] = max;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1299Test.java
@@ -0,0 +1,22 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/12 19:13
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1299Test {
+    @Test
+    public void test1() {
+        LeetCode1299 leetCode1299 = new LeetCode1299();
+        int[] arr = {17, 18, 5, 4, 6, 1};
+        int[] excepted = {18, 6, 6, 6, 1, -1};
+        Assert.assertArrayEquals(excepted, leetCode1299.replaceElements(arr));
+    }
+}
