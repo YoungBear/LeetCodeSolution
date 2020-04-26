e087386bbc6064efd8a8c130c4acf959f87237b4
YoungBear <youngbear@aliyun.com>
2020-04-26 22:32:36 +0800

    LeetCode 263. 丑数I
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode263.java
@@ -0,0 +1,29 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/26 22:30
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 263. 丑数
+ * 编写一个程序判断给定的数是否为丑数。
+ * 丑数就是只包含质因数 2, 3, 5 的正整数。
+ */
+public class LeetCode263 {
+    public boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode263Test.java
@@ -0,0 +1,22 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/26 22:31
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode263Test {
+    @Test
+    public void test1() {
+        LeetCode263 leetCode263 = new LeetCode263();
+        Assert.assertTrue(leetCode263.isUgly(6));
+        Assert.assertTrue(leetCode263.isUgly(8));
+        Assert.assertFalse(leetCode263.isUgly(14));
+    }
+}
