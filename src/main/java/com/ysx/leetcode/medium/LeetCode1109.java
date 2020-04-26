b83ea256aab9716a22c640ea2f2c78e6a00e2ecb
YoungBear <youngbear@aliyun.com>
2020-04-26 22:24:01 +0800

    LeetCode 1109. 航班预订统计
    等差数列
    
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode1109.java
@@ -0,0 +1,37 @@
+package com.ysx.leetcode.medium;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/26 22:21
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1109. 航班预订统计
+ * 等差数组
+ */
+public class LeetCode1109 {
+    /**
+     * 优化，使用等差数组，时间复杂度为O(n)
+     * 换一种思路理解题意，将问题转换为：某公交车共有 n 站，第 i 条记录 bookings[i] = [i, j, k] 表示在 i 站上车 k 人，乘坐到 j 站，在 j+1 站下车，需要按照车站顺序返回每一站车上的人数
+     * 根据 1 的思路，定义 counter[] 数组记录每站的人数变化，counter[i] 表示第 i+1 站。遍历 bookings[]：bookings[i] = [i, j, k] 表示在 i 站增加 k 人即
+     * counters[i-1] += k，在 j+1 站减少 k 人即 counters[j] -= k
+     * 遍历（整理）counter[] 数组，得到每站总人数： 每站的人数为前一站人数加上当前人数变化 counters[i] += counters[i - 1]
+     *
+     * @param bookings
+     * @param n
+     * @return
+     */
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] counters = new int[n];
+        for (int[] book : bookings) {
+            counters[book[0] - 1] += book[2];
+            if (book[1] < n) {
+                counters[book[1]] -= book[2];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            counters[i] += counters[i - 1];
+        }
+        return counters;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/medium/LeetCode1109Test.java
@@ -0,0 +1,23 @@
+package com.ysx.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/4/26 22:21
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1109Test {
+    @Test
+    public void test1() {
+        LeetCode1109 leetCode1109 = new LeetCode1109();
+        int[][] bookings = {{1, 2, 10}, {2, 3, 20}, {2, 5, 25}};
+        int n = 5;
+        int[] excepted = {10, 55, 45, 25, 25};
+        Assert.assertArrayEquals(excepted, leetCode1109.corpFlightBookings(bookings, n));
+    }
+}
