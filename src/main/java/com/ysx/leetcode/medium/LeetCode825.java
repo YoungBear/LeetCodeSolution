54ed20b4dc1b43b5591a177a75a3216cacefef77
YoungBear <youngbear@aliyun.com>
2020-05-07 22:59:13 +0800

    LeetCode 825. 适龄的朋友
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode825.java
@@ -0,0 +1,62 @@
+package com.ysx.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/5/7 22:24
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 825. 适龄的朋友
+ * 825. 适龄的朋友
+ * 当满足以下条件时，A 不能给 B（A、B不为同一人）发送好友请求：
+ * <p>
+ * age[B] <= 0.5 * age[A] + 7
+ * age[B] > age[A]
+ * age[B] > 100 && age[A] < 100
+ */
+public class LeetCode825 {
+    /**
+     * 双重循环，超时
+     * 使用map记录相同年龄的个数
+     * 执行用时 :22 ms, 在所有 Java 提交中击败了14.17%的用户
+     * 内存消耗 :42.4 MB, 在所有 Java 提交中击败了11.11%的用户
+     *
+     * @param ages
+     * @return
+     */
+    public int numFriendRequests(int[] ages) {
+        int result = 0;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int age : ages) {
+            countMap.put(age, countMap.getOrDefault(age, 0) + 1);
+        }
+        for (int key1 : countMap.keySet()) {
+            for (int key2 : countMap.keySet()) {
+                if (canSend(key1, key2)) {
+                    if (key1 == key2) {
+                        result += countMap.get(key1) * (countMap.get(key2) - 1);
+                    } else {
+                        result += countMap.get(key1) * countMap.get(key2);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean canSend(int ageA, int ageB) {
+        if (ageB <= (0.5 * ageA + 7)) {
+            return false;
+        }
+        if (ageB > 100 && ageA < 100) {
+            return false;
+        }
+        if (ageB > ageA) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/medium/LeetCode825Test.java
@@ -0,0 +1,38 @@
+package com.ysx.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/5/7 22:24
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode825Test {
+    @Test
+    public void test1() {
+        LeetCode825 leetCode825 = new LeetCode825();
+        int[] ages = {16, 16};
+        int excepted = 2;
+        Assert.assertEquals(excepted, leetCode825.numFriendRequests(ages));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode825 leetCode825 = new LeetCode825();
+        int[] ages = {16, 17, 18};
+        int excepted = 2;
+        Assert.assertEquals(excepted, leetCode825.numFriendRequests(ages));
+    }
+
+    @Test
+    public void test3() {
+        LeetCode825 leetCode825 = new LeetCode825();
+        int[] ages = {20, 30, 100, 110, 120};
+        int excepted = 3;
+        Assert.assertEquals(excepted, leetCode825.numFriendRequests(ages));
+    }
+}
