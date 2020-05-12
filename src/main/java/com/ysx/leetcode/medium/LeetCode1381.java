d4f69266e8a944d8a355c101537c11101240e5ca
YoungBear <youngbear@aliyun.com>
2020-05-12 21:46:11 +0800

    LeetCode 1381. 设计一个支持增量操作的栈
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode1381.java
@@ -0,0 +1,41 @@
+package com.ysx.leetcode.medium;
+
+import java.util.Stack;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/5/12 21:45
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1381. 设计一个支持增量操作的栈
+ * https://leetcode-cn.com/problems/design-a-stack-with-increment-operation/
+ */
+public class LeetCode1381 {
+    private int maxSize;
+    private Stack<Integer> stack;
+
+    public LeetCode1381(int maxSize) {
+        this.maxSize = maxSize;
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (stack.size() < maxSize) {
+            stack.push(x);
+        }
+    }
+
+    public int pop() {
+        if (stack.empty()) {
+            return -1;
+        }
+        return stack.pop();
+    }
+
+    public void increment(int k, int val) {
+        for (int i = 0; i < stack.size() && i < k; i++) {
+            stack.set(i, stack.get(i) + val);
+        }
+    }
+}
