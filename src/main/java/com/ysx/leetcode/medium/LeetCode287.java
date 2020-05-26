90934366aed151b7fba9b4160266223cb43d0aef
YoungBear <youngbear@aliyun.com>
2020-05-26 23:18:33 +0800

    LeetCode 287. 寻找重复数
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode287.java
@@ -0,0 +1,24 @@
+package com.ysx.leetcode.medium;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/5/26 23:16
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 287. 寻找重复数
+ */
+public class LeetCode287 {
+    public int findDuplicate(int[] nums) {
+        for (int i = 0;i < nums.length; i++) {
+            int index = Math.abs(nums[i]);
+            // 如果该位置元素已经被修改过，则index已经出现过一次
+            if (nums[index] < 0) {
+                return index;
+            } else {
+                nums[index] = - nums[index];
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/medium/LeetCode287Test.java
@@ -0,0 +1,30 @@
+package com.ysx.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/5/26 23:16
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode287Test {
+    @Test
+    public void test1() {
+        LeetCode287 leetCode287 = new LeetCode287();
+        int[] nums = {1, 3, 4, 2, 2};
+        int excepted = 2;
+        Assert.assertEquals(excepted, leetCode287.findDuplicate(nums));
+    }
+
+    @Test
+    public void test2() {
+        LeetCode287 leetCode287 = new LeetCode287();
+        int[] nums = {3, 1, 3, 4, 2};
+        int excepted = 3;
+        Assert.assertEquals(excepted, leetCode287.findDuplicate(nums));
+    }
+}
