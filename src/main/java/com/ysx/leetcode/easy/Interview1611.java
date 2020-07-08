ef3f4991a86fc826e76cf6d5873103e713cad54a
YoungBear <youngbear@aliyun.com>
2020-07-08 21:31:13 +0800

    面试题 16.11. 跳水板

---
+++ src/main/java/com/ysx/leetcode/easy/Interview1611.java
@@ -0,0 +1,65 @@
+package com.ysx.leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/7/8 21:03
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 面试题 16.11. 跳水板
+ * https://leetcode-cn.com/problems/diving-board-lcci/
+ */
+public class Interview1611 {
+
+    /**
+     * 使用数组
+     *
+     * @param shorter
+     * @param longer
+     * @param k
+     * @return
+     */
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (0 == k) {
+            return new int[0];
+        }
+        if (shorter == longer) {
+            return new int[]{shorter * k};
+        }
+        int[] result = new int[k + 1];
+        for (int i = 0; i < k + 1; i++) {
+            result[i] = shorter * (k - i) + longer * i;
+        }
+        return result;
+    }
+
+    /**
+     * 使用 set 超时
+     *
+     * @param shorter
+     * @param longer
+     * @param k
+     * @return
+     */
+    public int[] divingBoard1(int shorter, int longer, int k) {
+        if (0 == k) {
+            return new int[0];
+        }
+        Set<Integer> set = new HashSet<>();
+        set.add(shorter);
+        set.add(longer);
+        for (int i = 1; i < k; i++) {
+            Set<Integer> tempSet = new HashSet<>();
+            for (int num : set) {
+                tempSet.add(num + shorter);
+                tempSet.add(num + longer);
+            }
+            set.clear();
+            set.addAll(tempSet);
+        }
+        return set.stream().mapToInt(Integer::intValue).sorted().toArray();
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/Interview1611Test.java
@@ -0,0 +1,24 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/7/8 21:03
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class Interview1611Test {
+    @Test
+    public void test1() {
+        Interview1611 interview1611 = new Interview1611();
+        int shorter = 1;
+        int longer = 2;
+        int k = 3;
+        int[] excepted = {3, 4, 5, 6};
+        Assert.assertArrayEquals(excepted, interview1611.divingBoard(shorter, longer, k));
+    }
+}
