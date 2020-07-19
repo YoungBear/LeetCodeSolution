183188565ac3c9526aa064e49a7e108e8bc7b2cb
YoungBear <youngbear@aliyun.com>
2020-07-19 10:12:40 +0800

    LeetCode 1491. 去掉最低工资和最高工资后的工资平均值

---
+++ src/main/java/com/ysx/leetcode/easy/LeetCode1491.java
@@ -0,0 +1,29 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/7/19 10:01
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 1491. 去掉最低工资和最高工资后的工资平均值
+ * https://leetcode-cn.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+ */
+public class LeetCode1491 {
+    public double average(int[] salary) {
+        int sum = 0;
+        int max = salary[0];
+        int min = salary[0];
+        for (int sa : salary) {
+            sum += sa;
+            if (sa > max) {
+                max = sa;
+            }
+            if (sa < min) {
+                min = sa;
+            }
+        }
+        double result = (sum - max - min) / (double) (salary.length - 2);
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/easy/LeetCode1491Test.java
@@ -0,0 +1,46 @@
+package com.ysx.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/7/19 10:02
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode1491Test {
+    @Test
+    public void test1() {
+        LeetCode1491 leetCode1491 = new LeetCode1491();
+        int[] salary = {4000, 3000, 1000, 2000};
+        double excepted = 2500.00000d;
+        Assert.assertEquals(excepted, leetCode1491.average(salary), 0.000001d);
+    }
+
+    @Test
+    public void test2() {
+        LeetCode1491 leetCode1491 = new LeetCode1491();
+        int[] salary = {1000, 2000, 3000};
+        double excepted = 2000.00000d;
+        Assert.assertEquals(excepted, leetCode1491.average(salary), 0.000001d);
+    }
+
+    @Test
+    public void test3() {
+        LeetCode1491 leetCode1491 = new LeetCode1491();
+        int[] salary = {6000, 5000, 4000, 3000, 2000, 1000};
+        double excepted = 3500.00000d;
+        Assert.assertEquals(excepted, leetCode1491.average(salary), 0.000001d);
+    }
+
+    @Test
+    public void test4() {
+        LeetCode1491 leetCode1491 = new LeetCode1491();
+        int[] salary = {8000, 9000, 2000, 3000, 6000, 1000};
+        double excepted = 4750.00000d;
+        Assert.assertEquals(excepted, leetCode1491.average(salary), 0.000001d);
+    }
+}
