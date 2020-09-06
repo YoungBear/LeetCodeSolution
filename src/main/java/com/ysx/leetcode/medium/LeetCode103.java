0eb586cc2d2b5875dca2733229ad5f40fb2beb5b
YoungBear <youngbear@aliyun.com>
2020-09-06 10:14:55 +0800

    LeetCode 103. 二叉树的锯齿形层次遍历
    Signed-off-by: YoungBear <youngbear@aliyun.com>

---
+++ src/main/java/com/ysx/leetcode/medium/LeetCode103.java
@@ -0,0 +1,61 @@
+package com.ysx.leetcode.medium;
+
+import com.ysx.leetcode.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/9/6 9:54
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description 103. 二叉树的锯齿形层次遍历
+ * 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
+ */
+public class LeetCode103 {
+    /**
+     * 二叉树的锯齿形层次遍历
+     * 使用队列
+     *
+     * @param root 根节点
+     * @return 遍历列表
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        List<TreeNode> nodeList = new LinkedList<>();
+        nodeList.add(root);
+        // 标记是否翻转
+        boolean flag = true;
+        while (nodeList.size() > 0) {
+            List<Integer> list = new ArrayList<>();
+            List<TreeNode> temp = new LinkedList<>();
+            for (TreeNode node : nodeList) {
+                list.add(node.val);
+                if (node.left != null) {
+                    temp.add(node.left);
+                }
+                if (node.right != null) {
+                    temp.add(node.right);
+                }
+            }
+            if (flag) {
+                result.add(list);
+            } else {
+                Collections.reverse(list);
+                result.add(list);
+            }
+            nodeList.clear();
+            nodeList.addAll(temp);
+            flag = !flag;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ysx/leetcode/medium/LeetCode103Test.java
@@ -0,0 +1,45 @@
+package com.ysx.leetcode.medium;
+
+import com.ysx.leetcode.common.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2020/9/6 9:55
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class LeetCode103Test {
+    @Test
+    public void test1() {
+        LeetCode103 leetCode103 = new LeetCode103();
+        TreeNode root = new TreeNode(3);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n20 = new TreeNode(20);
+        TreeNode n15 = new TreeNode(15);
+        TreeNode n7 = new TreeNode(7);
+        root.left = n9;
+        root.right = n20;
+        n20.left = n15;
+        n20.right = n7;
+        List<List<Integer>> excepted = new ArrayList<>();
+        excepted.add(Arrays.asList(3));
+        excepted.add(Arrays.asList(20, 9));
+        excepted.add(Arrays.asList(15, 7));
+        List<List<Integer>> lists = leetCode103.zigzagLevelOrder(root);
+        Assert.assertEquals(excepted.size(), lists.size());
+        for (int i = 0; i < excepted.size(); i++) {
+            Assert.assertEquals(excepted.get(i).size(), lists.get(i).size());
+            for (int j = 0; j < excepted.get(i).size(); j++) {
+                Assert.assertEquals(excepted.get(i).get(j), lists.get(i).get(j));
+            }
+        }
+    }
+}
