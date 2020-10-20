74ac553affc7de0777e67a3aeca3c82cf620a649
YoungBear <youngbear@aliyun.com>
2020-10-20 22:18:24 +0800

    LeetCode 206. 反转链表
    Signed-off-by: YoungBear <youngbear@aliyun.com>

--- src/main/java/com/ysx/leetcode/easy/LeetCode206.java
+++ src/main/java/com/ysx/leetcode/easy/LeetCode206.java
@@ -18,6 +18,8 @@ import com.ysx.leetcode.common.ListNode;
 public class LeetCode206 {
     /**
      * 翻转链表
+     * 迭代法
+     *
      * @param head
      * @return
      */
@@ -35,5 +37,25 @@ public class LeetCode206 {
             cur = next;
         }
         return prev;
+    }
+
+    /**
+     * 翻转链表
+     * 递归法
+     * 假设列表的其余部分已经被反转，现在我们应该如何反转它前面的部分
+     *
+     * @param head 头结点
+     * @return 翻转后的链表
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 反转第二个节点
+        ListNode p = reverseList2(head.next);
+        // 反转第一个节点
+        head.next.next = head;
+        head.next = null;
+        return p;
     }
 }
